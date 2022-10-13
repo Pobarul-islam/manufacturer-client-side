33e146c71c9233d99e0ca55e0671dcb826f45d2c
Pobarul-islam <pobarul3@gmail.com>
2022-10-13 18:22:57 +0600

    loading component changed

--- src/Pages/Shared/Loading.js
+++ src/Pages/Shared/Loading.js
@@ -2,10 +2,9 @@ import React from 'react';
 
 const Loading = () => {
     return (
-        <div className="flex items-center justify-center ">
-            <div className=""></div>
-            <button className="btn loading bg-primary mt-20">loading</button>
-        </div>
+      <div className="flex items-center justify-center ">
+        <div className="w-16 h-16 mt-36 border-4 border-dashed rounded-full animate-spin  border-violet-700"></div>
+      </div>
     );
 };
 
